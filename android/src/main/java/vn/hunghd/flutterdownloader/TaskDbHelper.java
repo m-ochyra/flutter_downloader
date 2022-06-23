d530defd48a90f97afa6b79fe678e81f2b1dbc35
Marcin Ochyra <m.ochyra@ideo.pl>
2022-06-23 10:17:31 +0200

    Bump DB version

--- android/src/main/java/vn/hunghd/flutterdownloader/TaskDbHelper.java
+++ android/src/main/java/vn/hunghd/flutterdownloader/TaskDbHelper.java
@@ -7,7 +7,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 import vn.hunghd.flutterdownloader.TaskContract.TaskEntry;
 
 public class TaskDbHelper extends SQLiteOpenHelper {
-    public static final int DATABASE_VERSION = 3;
+    public static final int DATABASE_VERSION = 4;
     public static final String DATABASE_NAME = "download_tasks.db";
 
     private static TaskDbHelper instance = null;
